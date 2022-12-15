3f804843128300758d700611fe2be40ae555188b
IrodaMuminova <irodamuminova899@gmail.com>
2022-12-15 18:18:42 +0500

    initial

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 let body = document.querySelector('body');
 let header = document.querySelector('h1');
-let mavzu_button = document.getElementById('mavzu');
+let mavzu_button = document.getElementById('fon');
 mavzu_button.addEventListener('click', function (evt) {
     body.classList.toggle('dark')
     header.classList.toggle('dark')
